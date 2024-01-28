a927ef9fa5f38d8f0f1e8039ac2e4fea22b7c3ae
Arthur Cunha <arthurdaconceicaocunha@gmail.com>
2024-01-28 11:21:53 -0300

    feat: adicionando módulos de typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { config } from './ormconfig';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, TypeOrmModule.forRoot(config)],
   controllers: [AppController],
   providers: [AppService],
 })
