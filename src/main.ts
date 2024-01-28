1f813db2b304cd3e7247e84367818f6b9dd8de30
Arthur Cunha <arthurdaconceicaocunha@gmail.com>
2024-01-28 11:20:52 -0300

    feat: adicionando swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
+
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('User API')
+    .setDescription('Dental Dash API for managing users')
+    .setVersion('1.0')
+    .addTag('users')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
