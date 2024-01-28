ecaffbc1fe79173040180c93cdeb46fe280864ee
Arthur Cunha <arthurdaconceicaocunha@gmail.com>
2024-01-28 11:26:39 -0300

    :sparkles: feat: adicionando entidades

---
+++ src/users/entities/user.entity.ts
@@ -0,0 +1,13 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn('increment')
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column()
+  email: string;
+}
