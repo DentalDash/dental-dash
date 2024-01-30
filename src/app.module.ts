d64ab970e5829dd74bd3e20b51d4a7b8234b1cda
Arthur Cunha <arthurdaconceicaocunha@gmail.com>
2024-01-30 16:41:31 -0300

    Refactor app.module.ts to use updated TypeORM configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { config } from './ormconfig';
-import { UsersModule } from './users/users.module';
+import { typeOrmConfig } from './ormconfig';
 
 @Module({
-  imports: [UsersModule, TypeOrmModule.forRoot(config)],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [TypeOrmModule.forRoot(typeOrmConfig)],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
