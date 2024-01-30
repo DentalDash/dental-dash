295ba30770703dfbf60b907811dd44142904e6ae
Arthur Cunha <arthurdaconceicaocunha@gmail.com>
2024-01-30 16:41:55 -0300

    Add UserRepository class with createUser method

---
+++ src/users/user.repository.ts
@@ -0,0 +1,46 @@
+import { Repository } from 'typeorm';
+import { User } from './entities/user.entity';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UserRole } from './user.role';
+import * as bcrypt from 'bcrypt';
+import * as crypto from 'crypto';
+import { ConflictException, Injectable, InternalServerErrorException } from '@nestjs/common';
+
+
+@Injectable()
+
+export class UserRepository extends Repository<User> {
+  async createUser(
+    createUserDto: CreateUserDto,
+    role: UserRole,
+  ): Promise<User> {
+    const { email, name, password } = createUserDto;
+
+    const user = this.create();
+    user.email = email;
+    user.name = name;
+    user.role = role;
+    user.status = true;
+    user.confirmationToken = crypto.randomBytes(32).toString('hex');
+    user.salt = await bcrypt.genSalt();
+    user.password = await this.hashPassword(password, user.salt);
+    try {
+      await user.save();
+      delete user.password;
+      delete user.salt;
+      return user;
+    } catch (error) {
+      if (error.code.toString() === '23505') {
+        throw new ConflictException('Endereço de email já está em uso');
+      } else {
+        throw new InternalServerErrorException(
+          'Erro ao salvar o usuário no banco de dados',
+        );
+      }
+    }
+  }
+
+  private async hashPassword(password: string, salt: string): Promise<string> {
+    return bcrypt.hash(password, salt);
+  }
+}
