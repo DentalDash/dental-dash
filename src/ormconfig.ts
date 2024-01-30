00a4d66a13a49a5a61b0bf0e203506bd12d4f970
Arthur Cunha <arthurdaconceicaocunha@gmail.com>
2024-01-30 16:41:40 -0300

    Update database configuration to use PostgreSQL

--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -1,8 +1,12 @@
-import { DataSourceOptions } from 'typeorm';
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
-export const config: DataSourceOptions = {
-  type: 'sqlite',
-  database: '.db/sql',
+export const typeOrmConfig: TypeOrmModuleOptions = {
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'pguser',
+  password: 'pgpassword',
+  database: 'dental_dash',
+  entities: ["dist/**/*.entity{.ts,.js}"],
   synchronize: true,
-  entities: [__dirname + '/**/*.entity{.ts,.js}'],
 };
