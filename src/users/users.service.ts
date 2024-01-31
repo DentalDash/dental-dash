0fd3d61219e1a740a00b63b69ec7367e0bd2a0d6
Gustaxz <guxtavogusteible@gmail.com>
2024-01-31 12:38:13 -0300

    fix: o repository estava salvando a entidade errada

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,5 +1,7 @@
 import { Injectable, UnprocessableEntityException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import * as bcrypt from 'bcrypt';
+import * as crypto from 'crypto';
 import { Repository } from 'typeorm';
 import { CreateUserDto } from './dto/create-user.dto';
 import { User } from './entities/user.entity';
@@ -16,9 +18,19 @@ export class UsersService {
     if (createUserDto.password != createUserDto.passwordConfirmation) {
       throw new UnprocessableEntityException('As senhas não conferem');
     } else {
-      const user = this.usersRepository.create(createUserDto);
-      user.role = UserRole.ADMIN;
-      return this.usersRepository.save(user);
+      const createUser = new User();
+
+      const salt = await bcrypt.genSalt();
+
+      createUser.email = createUserDto.email;
+      createUser.name = createUserDto.name;
+      createUser.role = UserRole.ADMIN;
+      createUser.status = true;
+      createUser.salt = salt;
+      createUser.password = await bcrypt.hash(createUserDto.password, salt);
+      createUser.confirmationToken = crypto.randomBytes(32).toString('hex');
+
+      return this.usersRepository.save(createUser);
     }
   }
 }
