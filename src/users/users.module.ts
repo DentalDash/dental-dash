587767b46bea3d0cd4480fb24900b56231910081
Arthur Cunha <arthurdaconceicaocunha@gmail.com>
2024-02-04 11:43:35 -0300

    Refactor users module imports and add Passport authentication

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,12 +1,16 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from './entities/user.entity';
-import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { UsersController } from './users.controller';
+import { PassportModule } from '@nestjs/passport';
+import { User } from './entities/user.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
-  controllers: [UsersController],
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+  ],
   providers: [UsersService],
+  controllers: [UsersController],
 })
 export class UsersModule {}
