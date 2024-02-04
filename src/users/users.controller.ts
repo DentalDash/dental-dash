24dcece24fb4625492f5c0d0ebf54765ad568477
Arthur Cunha <arthurdaconceicaocunha@gmail.com>
2024-02-04 11:43:31 -0300

    Add authentication and authorization to createAdminUser endpoint

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,17 +1,29 @@
-import { Body, Controller, Post, ValidationPipe } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  ValidationPipe,
+  UseGuards,
+} from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
-import { ReturnUserDto } from './dto/return_user.dto';
 import { UsersService } from './users.service';
+import { ReturnUserDto } from './dto/return-user.dto';
+import { AuthGuard } from '@nestjs/passport';
+import { RolesGuard } from '../auth/auth.guard';
+import { Role } from '../auth/auth.role';
+import { UserRole } from './user.role';
 
 @Controller('users')
 export class UsersController {
-  constructor(private userServices: UsersService) {}
+  constructor(private usersService: UsersService) {}
 
   @Post()
+  @Role(UserRole.ADMIN)
+  @UseGuards(AuthGuard(), RolesGuard)
   async createAdminUser(
     @Body(ValidationPipe) createUserDto: CreateUserDto,
-    ): Promise<ReturnUserDto> {
-    const user = await this.userServices.createAdminUser(createUserDto);
+  ): Promise<ReturnUserDto> {
+    const user = await this.usersService.createAdminUser(createUserDto);
     return {
       user,
       message: 'Administrador cadastrado com sucesso',
