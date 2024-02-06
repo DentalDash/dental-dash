c1cd7ee9bdf64a9dc50166f64b4164ec823d8205
Arthur Cunha <arthurdaconceicaocunha@gmail.com>
2024-02-06 19:08:03 -0300

    Update UpdateUserDto with validation and optional fields

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,4 +1,25 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateUserDto } from './create-user.dto';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+import { IsString, IsEmail, IsOptional } from 'class-validator';
+import { UserRole } from '../user.role';
+export class UpdateUserDto {
+  @IsOptional()
+  @IsString({
+    message: 'Informe um nome de usuário válido',
+  })
+  name: string;
+
+  @IsOptional()
+  @IsEmail(
+    {},
+    {
+      message: 'Informe um endereço de email válido',
+    },
+  )
+  email: string;
+
+  @IsOptional()
+  role: UserRole;
+
+  @IsOptional()
+  status: boolean;
+}
