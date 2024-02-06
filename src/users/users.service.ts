9524777c0e4406dbc72e6dd116244c327c044715
Arthur Cunha <arthurdaconceicaocunha@gmail.com>
2024-02-06 19:07:51 -0300

    Update users.service.ts with new methods and DTOs

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, UnprocessableEntityException } from '@nestjs/common';
+import { ForbiddenException, Injectable, InternalServerErrorException, NotFoundException, UnprocessableEntityException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import * as bcrypt from 'bcrypt';
 import * as crypto from 'crypto';
@@ -6,6 +6,8 @@ import { Repository } from 'typeorm';
 import { CreateUserDto } from './dto/create-user.dto';
 import { User } from './entities/user.entity';
 import { UserRole } from './user.role';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { FindUsersQueryDto } from './dto/find-user.dto';
 
 @Injectable()
 export class UsersService {
@@ -32,5 +34,84 @@ export class UsersService {
 
       return this.usersRepository.save(createUser);
     }
+  }
+
+  async findUserById(userId: string): Promise<User> {
+    const user = await this.usersRepository.findOne({ where: { id: userId } });
+
+    if (!user) throw new NotFoundException('Usuário não encontrado');
+
+    return user;
+  }
+
+  async updateUser(updateUserDto: UpdateUserDto, id: string, user: User): Promise<User> {
+    // Verificar se o usuário tem permissão para atualizar com base no papel
+
+    const userToUpdate = await this.findUserById(id);
+    const { name, email, role, status } = updateUserDto;
+    // Atualizar apenas os campos fornecidos no DTO
+    if (name) {
+        userToUpdate.name = name;
+    }
+    if (email) {
+        userToUpdate.email = email;
+    }
+    if (role) {
+        // Verificar se o usuário tem permissão para atribuir o papel
+        if (user.role !== UserRole.ADMIN) {
+            throw new ForbiddenException('Você não tem permissão para atribuir papéis');
+        }
+        userToUpdate.role = role;
+    }
+    if (status !== undefined) {
+        userToUpdate.status = status;
+    }
+
+    try {
+        await userToUpdate.save();
+        return userToUpdate;
+    } catch (error) {
+        throw new InternalServerErrorException('Erro ao salvar os dados no banco de dados');
+    }
+}
+
+async deleteUser(userId: string) {
+  const result = await this.usersRepository.delete({ id: userId });
+  if (result.affected === 0) {
+    throw new NotFoundException(
+      'Não foi encontrado um usuário com o ID informado',
+    );
+    }
+  }
+
+async findUsers(queryDto: FindUsersQueryDto): Promise<{ users: User[]; total: number }> {
+    queryDto.status = queryDto.status === undefined ? true : queryDto.status;
+    queryDto.page = queryDto.page < 1 ? 1 : queryDto.page;
+    queryDto.limit = queryDto.limit > 100 ? 100 : queryDto.limit;
+  
+    const { email, name, status, role } = queryDto;
+    const queryBuilder = this.usersRepository.createQueryBuilder('user');
+    queryBuilder.where('user.status = :status', { status });
+  
+    if (email) {
+      queryBuilder.andWhere('user.email ILIKE :email', { email: `%${email}%` });
+    }
+  
+    if (name) {
+      queryBuilder.andWhere('user.name ILIKE :name', { name: `%${name}%` });
+    }
+  
+    if (role) {
+      queryBuilder.andWhere('user.role = :role', { role });
+    }
+  
+    queryBuilder.skip((queryDto.page - 1) * queryDto.limit);
+    queryBuilder.take(queryDto.limit);
+    queryBuilder.orderBy(queryDto.sort ? JSON.parse(queryDto.sort) : undefined);
+    queryBuilder.select(['user.name', 'user.email', 'user.role', 'user.status']);
+  
+    const [users, total] = await queryBuilder.getManyAndCount();
+  
+    return { users, total };
   }
 }
