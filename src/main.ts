7eebdc51e4a485e04f1944997ee65b0dc566d588
Arthur Cunha <arthurdaconceicaocunha@gmail.com>
2024-02-12 13:16:46 -0300

    :sparkles: feat: Add winston logging configuration

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,11 @@
+import {
+  utilities as nestWinstonModuleUtilities,
+  WinstonModuleOptions,
+} from 'nest-winston';
+import * as winston from 'winston';
+
+
+
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
@@ -20,3 +28,21 @@ async function bootstrap() {
   await app.listen(process.env.PORT);
 }
 bootstrap();
+
+export const winstonConfig: WinstonModuleOptions = {
+  levels: winston.config.npm.levels,
+  level: 'verbose',
+  transports: [
+    new winston.transports.Console({
+      format: winston.format.combine(
+        winston.format.timestamp(),
+        nestWinstonModuleUtilities.format.nestLike(),
+      ),
+    }),
+    new winston.transports.File({
+      level: 'verbose',
+      filename: 'application.log',
+      dirname: 'logs',
+    }),
+  ],
+};
