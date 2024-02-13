a92e9af0183b95efe379582e844ae57d58127a07
Arthur Cunha <arthurdaconceicaocunha@gmail.com>
2024-02-13 14:05:26 -0300

    Add mailer configuration file

---
+++ src/configs/mail-config.ts
@@ -0,0 +1,23 @@
+import { MailerOptions } from '@nestjs-modules/mailer';
+import * as path from 'path';
+import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
+
+export const mailerConfig: MailerOptions = {
+  template: {
+    dir: path.resolve(__dirname, '..', '..', 'templates'),
+    adapter: new HandlebarsAdapter(), 
+    options: {
+      extName: '.hbs',
+      layoutsDir: path.resolve(__dirname, '..', '..', 'templates'),
+    },
+  },
+  transport: {
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
+    auth: {
+      user: 'arthurdaconceicaocunha@gmail.com',
+      pass: 'kjtc aymn whux bcra',
+    },
+  },
+};
