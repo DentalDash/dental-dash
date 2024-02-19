57c3c9ce849011c0613aa48909b24d6a1cf0108e
Arthur Cunha <arthurdaconceicaocunha@gmail.com>
2024-02-19 11:48:54 -0300

    Update mailerConfig in mail-config.ts

--- src/configs/mail-config.ts
+++ src/configs/mail-config.ts
@@ -3,12 +3,14 @@ import * as path from 'path';
 import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
 
 export const mailerConfig: MailerOptions = {
+ 
   template: {
     dir: path.resolve(__dirname, '..', '..', 'templates'),
+
+
     adapter: new HandlebarsAdapter(), 
     options: {
-      extName: '.hbs',
-      layoutsDir: path.resolve(__dirname, '..', '..', 'templates'),
+      stric: true
     },
   },
   transport: {
@@ -19,8 +21,11 @@ export const mailerConfig: MailerOptions = {
       user: 'arthurdaconceicaocunha@gmail.com',
       pass: 'kjtc aymn whux bcra',
     },
+    
+
   },
+
 };
 
-const layoutsDir = mailerConfig.template.options.layoutsDir;
-console.log(layoutsDir);
+
+
