6d8ff99a4339660a24b2e07d423539577bd4240a
Arthur Cunha <arthurdaconceicaocunha@gmail.com>
2024-02-26 15:26:13 -0300

    Add CRUD operations for supplies service

--- src/suplies/supplies.service.ts
+++ src/suplies/supplies.service.ts
@@ -1,7 +1,12 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  InternalServerErrorException,
+  NotFoundException,
+} from '@nestjs/common';
 import { SuppliesRepository } from './supplies.repository';
 import { CreateSuppliesDto } from './dto/create-supplies.dto';
 import { Supplies } from './entities/supplies.entity';
+import { UpdateSuppliesDto } from './dto/update-supplies.dto';
 
 @Injectable()
 export class SuppliesService {
@@ -11,5 +16,55 @@ export class SuppliesService {
     createSuppliesDto: CreateSuppliesDto,
   ): Promise<Supplies> {
     return this.suppliesRepository.createSupplies(createSuppliesDto);
+  }
+
+  async deleteSupplies(suppliesId: string) {
+    const result = await this.suppliesRepository.delete({ id: suppliesId });
+    if (result.affected === 0) {
+      throw new NotFoundException(
+        'Não foi encontrado um usuário com o ID informado',
+      );
+    }
+  }
+
+  async findSuppliesById(suppliesId: string): Promise<Supplies> {
+    const supplie = await this.suppliesRepository.findOne({
+      where: { id: suppliesId },
+    });
+
+    if (!supplie) throw new NotFoundException('Insumo não encontrado');
+
+    return supplie;
+  }
+
+  async updateSupplies(
+    updateSuppliesDto: UpdateSuppliesDto,
+    id: string,
+  ): Promise<Supplies> {
+    const suppilesToUpdate = await this.findSuppliesById(id);
+    const { name, description, isConsumable, materialType } = updateSuppliesDto;
+
+    if (name) {
+      suppilesToUpdate.name = name;
+    }
+    if (description) {
+      suppilesToUpdate.description = description;
+    }
+    if (isConsumable) {
+      suppilesToUpdate.isConsumable = isConsumable;
+
+      if (materialType) {
+        suppilesToUpdate.materialType = materialType;
+      }
+
+      try {
+        await suppilesToUpdate.save();
+        return suppilesToUpdate;
+      } catch (error) {
+        throw new InternalServerErrorException(
+          'Erro ao salvar os dados no banco de dados',
+        );
+      }
+    }
   }
 }
