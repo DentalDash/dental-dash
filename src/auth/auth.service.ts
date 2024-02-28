9804dde3406285af8a56f9f0f28e55403ad795f6
Arthur Cunha <arthurdaconceicaocunha@gmail.com>
2024-02-28 13:31:34 -0300

    feat: adding email verification

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,5 +1,7 @@
 import {
+  BadRequestException,
   Injectable,
+  InternalServerErrorException,
   NotFoundException,
   UnauthorizedException,
   UnprocessableEntityException,
@@ -29,44 +31,55 @@ export class AuthService {
     });
   }
   async signUp(createUserDto: CreateUserDto): Promise<User> {
-    if (createUserDto.password !== createUserDto.passwordConfirmation) {
-      throw new UnprocessableEntityException('As senhas não conferem');
-    }
-
-    const salt = await bcrypt.genSalt(); // Gerar salt
-    const hashedPassword = await bcrypt.hash(createUserDto.password, salt);
-
-    const newUser = this.usersRepository.create({
-      ...createUserDto,
-      role: UserRole.USER,
-      salt: salt,
-      password: hashedPassword,
-    });
-
-    // Gerar o Token de confirmação de e-mail
-    const confirmationToken = randomBytes(32).toString('hex');
-    newUser.confirmationToken = confirmationToken;
-    await this.usersRepository.save(newUser);
-
-    // Enviar e-mail de confirmação
-    const mail = {
-      to: newUser.email,
-      from: 'noreply@application.com',
-      subject: 'Email de confirmação',
-      template: 'email-confirmation',
-
-      context: {
-        token: newUser.confirmationToken,
-      },
-    };
-
     try {
-      await this.mailerService.sendMail(mail);
-    } catch (error) {
-      throw new Error('Erro ao enviar e-mail de confirmação');
-    }
+      if (createUserDto.password !== createUserDto.passwordConfirmation) {
+        throw new UnprocessableEntityException('As senhas não conferem');
+      }
+      const salt = await bcrypt.genSalt(); // Gerar salt
+      const hashedPassword = await bcrypt.hash(createUserDto.password, salt);
 
-    return newUser;
+      const newUser = this.usersRepository.create({
+        ...createUserDto,
+        role: UserRole.USER,
+        salt: salt,
+        password: hashedPassword,
+      });
+
+      // Gerar o Token de confirmação de e-mail
+      const confirmationToken = randomBytes(32).toString('hex');
+      newUser.confirmationToken = confirmationToken;
+      await this.usersRepository.save(newUser);
+
+      // Enviar e-mail de confirmação
+      const mail = {
+        to: newUser.email,
+        from: 'noreply@application.com',
+        subject: 'Email de confirmação',
+        template: 'email-confirmation',
+
+        context: {
+          token: newUser.confirmationToken,
+        },
+      };
+
+      try {
+        await this.mailerService.sendMail(mail);
+      } catch (error) {
+        throw new Error('Erro ao enviar e-mail de confirmação');
+      }
+
+      return newUser;
+    } catch (error) {
+      if (
+        error.detail &&
+        /(?:email)[\s\S]+(?:already exists)/.test(error.detail)
+      ) {
+        throw new BadRequestException('E-mail já está em uso');
+      }
+      throw new InternalServerErrorException(
+        'Erro ao salvar o usuário no banco de dados',
+      );
+    }
   }
 
   async signIn(loginDto: LoginDto): Promise<{ token: string }> {
