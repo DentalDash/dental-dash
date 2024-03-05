cfc047868c99b43d9b23894f29c90d707eeee1ee
Arthur Cunha <arthurdaconceicaocunha@gmail.com>
2024-03-05 09:41:30 -0300

    feat: adding deleteProcedures function

--- src/procedures/procedures.service.ts
+++ src/procedures/procedures.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateProcedureDto } from './dto/create-procedures.dto';
 import { Procedure } from './entities/procedures.entity';
 import { ProceduresRepository } from './procedures.repository';
@@ -10,5 +10,14 @@ export class ProceduresService {
     createProcedureDto: CreateProcedureDto,
   ): Promise<Procedure> {
     return this.proceduresRepository.createProcedure(createProcedureDto);
+  }
+
+  async deleteProcedures(procedureId: string) {
+    const result = await this.proceduresRepository.delete({ id: procedureId });
+    if (result.affected === 0) {
+      throw new NotFoundException(
+        'Não foi encontrado um usuário com o ID informado',
+      );
+    }
   }
 }
