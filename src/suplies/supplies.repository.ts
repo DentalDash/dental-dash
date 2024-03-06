02f4a44d729cef4eb32db6cc9ea85216d3a19162
Arthur Cunha <arthurdaconceicaocunha@gmail.com>
2024-03-06 10:45:42 -0300

    feat: validating supplies

--- src/suplies/supplies.repository.ts
+++ src/suplies/supplies.repository.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { DataSource, Repository } from 'typeorm';
 import { Supplies } from './entities/supplies.entity';
 import { CreateSuppliesDto } from './dto/create-supplies.dto';
@@ -8,15 +8,26 @@ export class SuppliesRepository extends Repository<Supplies> {
   constructor(private dataSource: DataSource) {
     super(Supplies, dataSource.createEntityManager());
   }
-
   async createSupplies(
     createSuppliesDto: CreateSuppliesDto,
   ): Promise<Supplies> {
+    const existingSupplies = await this.findOne({
+      where: {
+        name: createSuppliesDto.name.toLowerCase().normalize('NFD'),
+      },
+    });
+
+    if (existingSupplies) {
+      throw new NotFoundException(`O insumo já está cadastrado!`);
+    }
+
     const supplies = new Supplies();
-    supplies.name = createSuppliesDto.name;
+    supplies.name = createSuppliesDto.name.toLowerCase().normalize('NFD');
+
     supplies.description = createSuppliesDto.description;
     supplies.isConsumable = createSuppliesDto.isConsumable;
     supplies.materialType = createSuppliesDto.materialType;
+
     return this.save(supplies);
   }
 }
