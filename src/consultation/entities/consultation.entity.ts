94b867b946cab89578c053c90408312653c7b44a
arthcc <arthurdaconceicaocunha@gmail.com>
2024-04-01 16:04:26 -0300

    Add Consultation entity

---
+++ src/consultation/entities/consultation.entity.ts
@@ -0,0 +1,39 @@
+import { BaseEntity, Column, Entity, PrimaryGeneratedColumn, ManyToOne, ManyToMany, JoinTable } from 'typeorm';
+import { Dentist } from 'src/dentist/entities/dentist.entity';
+import { Patient } from 'src/patients/entities/patient.entity';
+import { Procedure } from 'src/procedures/entities/procedures.entity';
+import { Supplies } from 'src/suplies/entities/supplies.entity';
+
+@Entity('consultation')
+export class Consultation extends BaseEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ nullable: false, type: 'timestamp' })
+  date_time: Date;
+
+  @ManyToOne(() => Dentist, { eager: true }) 
+  dentist: Dentist;
+
+  @ManyToOne(() => Patient, { eager: true })
+  patient: Patient;
+
+  @ManyToMany(() => Procedure) 
+  @JoinTable({
+    name: 'consultation_procedure',
+    joinColumn: { name: 'consultation_id', referencedColumnName: 'id' },
+    inverseJoinColumn: { name: 'procedure_id', referencedColumnName: 'id' },
+  })
+  procedure: Procedure[];
+
+  @ManyToMany(() => Supplies) 
+  @JoinTable({
+    name: 'consultation_supplies',
+    joinColumn: { name: 'consultation_id', referencedColumnName: 'id' },
+    inverseJoinColumn: { name: 'supplies_id', referencedColumnName: 'id' },
+  })
+  supplies: Supplies[];
+
+  @Column({ nullable: true, type: 'text' })
+  oservations: string;
+}
