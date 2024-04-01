c4ef64e53d6f133320fe6a30702e3ad4fe8ee46c
arthcc <arthurdaconceicaocunha@gmail.com>
2024-04-01 16:04:21 -0300

    Add CreateConsultationDto class for consultation creation

---
+++ src/consultation/dto/create.consultation.dto.ts
@@ -0,0 +1,42 @@
+import { IsNotEmpty } from "class-validator";
+import { Dentist } from "src/dentist/entities/dentist.entity";
+import { Patient } from "src/patients/entities/patient.entity";
+import { Procedure } from "src/procedures/entities/procedures.entity";
+import { Supplies } from "src/suplies/entities/supplies.entity";
+
+export class CreateConsultationDto {
+    @IsNotEmpty(
+        { message: 'Informe a data e hora da consulta' }
+    
+    )
+    date_time: Date;
+
+    @IsNotEmpty(
+        { message: 'Informe o dentista' }
+    )
+    dentistData: Dentist;
+
+    @IsNotEmpty(
+        { message: 'Informe o paciente' }
+    )
+    @IsNotEmpty(
+        { message: 'Informe o paciente' }
+    )    
+    patientData: Patient;
+
+    @IsNotEmpty(
+        { message: 'Informe o procedimento' }
+    )
+    procedure: Procedure[];
+
+    @IsNotEmpty(
+        { message: 'Informe o material' }
+    )
+    suppliesNames: Supplies[];
+
+
+    @IsNotEmpty(
+        { message: 'Informe as observações' }
+    )
+    oservations: string;
+}
