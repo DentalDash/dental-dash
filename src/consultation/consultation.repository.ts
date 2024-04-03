943411e0ba509c2aa545471dcab9c5133e67dd79
arthcc <arthurdaconceicaocunha@gmail.com>
2024-04-03 11:02:09 -0300

    feat: Add imports and update createConsultation method

--- src/consultation/consultation.repository.ts
+++ src/consultation/consultation.repository.ts
@@ -2,6 +2,10 @@ import { Injectable, NotFoundException } from "@nestjs/common";
 import { DataSource, EntityManager, Repository } from "typeorm";
 import { Consultation } from "./entities/consultation.entity";
 import { CreateConsultationDto } from "./dto/create.consultation.dto";
+import { Supplies } from "src/suplies/entities/supplies.entity";
+import { Procedure } from "src/procedures/entities/procedures.entity";
+import { Patient } from "src/patients/entities/patient.entity";
+import { Dentist } from "src/dentist/entities/dentist.entity";
 
 
 @Injectable()
@@ -12,7 +16,6 @@ export class ConsultationRepository extends Repository<Consultation> {
     }
     private entityManager: EntityManager;
     
-
     async createConsultation(createConsultationDto: CreateConsultationDto): Promise<Consultation> {
         const {
             date_time,
@@ -22,61 +25,63 @@ export class ConsultationRepository extends Repository<Consultation> {
             suppliesNames,
             oservations
         } = createConsultationDto;
-
-            const Supplies = await Promise.all(
+    
+        const supplies = await Promise.all(
             suppliesNames.map(async (supplyName) => {
-                    const supply = await this.entityManager.findOne(Supplies, {
-                        where: { name: supplyName },
-                    });
-                    if (!supply) {
-                        throw new NotFoundException(`Insumo ${supplyName} não encontrado, verifique no cadastro e tente novamente!`);
-                    }
-                    return supply;
-                }));
-
-            const Procedure = await Promise.all(
+                const supply = await this.entityManager.findOne(Supplies, {
+                    where: { name: supplyName.name }, 
+                });
+                if (!supply) {
+                    throw new NotFoundException(`Insumo ${supplyName.name} não encontrado, verifique no cadastro e tente novamente!`);
+                }
+                return supply;
+            })
+        );
+    
+        const procedures = await Promise.all(
             procedure.map(async (procedureName) => {
                 const procedure = await this.entityManager.findOne(Procedure, {
-                    where: { name: procedureName },
+                    where: { name: procedureName.name },
                 });
                 if (!procedure) {
-                    throw new NotFoundException(`Procedimento ${procedureName} não encontrado, verifique no cadastro e tente novamente!`);
+                    throw new NotFoundException(`Procedimento ${procedureName.name} não encontrado, verifique no cadastro e tente novamente!`);
                 }
                 return procedure;
-            }
-        ));
-            
-            const Patient = await Promise.all(
-            [patientData].map(async (cpf) => { 
+            })
+        );
+    
+        const patients = await Promise.all(
+            [patientData.cpf].map(async (cpf) => { 
                 const patient = await this.entityManager.findOne(Patient, {
                     where: { cpf: cpf },
                 });
                 if (!patient) {
-                    throw new NotFoundException(`Paciente ${cpf} não encontrado, verifique no cadastro e tente novamente!`);
+                    throw new NotFoundException(`Paciente não encontrado, verifique no cadastro e tente novamente!`);
                 }
                 return patient;
             })
         );
-            const Dentist = await Promise.all(
-            [dentistData].map(async (croNumber) => {
+    
+        const dentists = await Promise.all(
+            [dentistData.croNumber].map(async (croNumber) => {
                 const dentist = await this.entityManager.findOne(Dentist, {
-                    where: { cro: croNumber },
+                    where: { croNumber: croNumber },
                 });
                 if (!dentist) {
                     throw new NotFoundException(`Dentista ${croNumber} não encontrado, verifique no cadastro e tente novamente!`);
                 }
                 return dentist;
-            }));
+            })
+        );
 
         const consultation = new Consultation();
         consultation.date_time = date_time;
-        consultation.dentist = dentistData;
-        consultation.patient = patientData;
-        consultation.procedure = procedure;
-        consultation.supplies = suppliesNames;
+        consultation.dentist = dentists[0]; 
+        consultation.patient = patients[0]; 
+        consultation.procedure = procedures;
+        consultation.supplies = supplies;
         consultation.oservations = oservations;
-
+    
         return this.save(consultation);
-
     }
-}
+}    
