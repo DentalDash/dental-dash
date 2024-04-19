eb13a615a2831831526af6a49d49550f47012dff
arthcc <arthurdaconceicaocunha@gmail.com>
2024-04-19 13:42:23 -0300

    Add deleteConsultation method to ConsultationService

--- src/consultation/consultation.service.ts
+++ src/consultation/consultation.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, NotFoundException } from "@nestjs/common";
 import { ConsultationRepository } from "./consultation.repository";
 import { CreateConsultationDto } from "./dto/create.consultation.dto";
 import { Consultation } from "./entities/consultation.entity";
@@ -10,5 +10,13 @@ export class ConsultationService{
 
     async createConsultation(createConsultationDto: CreateConsultationDto): Promise<Consultation> {
         return this.consultationRepository.createConsultation(createConsultationDto);
+    }
+
+    async deleteConsultation(id: string) {
+        const result = await this.consultationRepository.delete({ id: id });
+        if (result.affected === 0) {
+            throw new NotFoundException(
+                'Não foi encontrado uma consulta com os dados informados');
+        }
     }
 }
